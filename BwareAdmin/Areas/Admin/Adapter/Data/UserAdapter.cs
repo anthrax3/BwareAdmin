5f52101e4a06d6c115d7064d16c46d9d932ade2b
James D <jdsv650@yahoo.com>
2016-01-16 21:59:07 -0500

    Add User Controller, UserAdapter (implements IuserAdapter), Index action pulls in all users and displays user name, Add UserViewModel and Index View

---
+++ BwareAdmin/Areas/Admin/Adapter/Data/UserAdapter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BwareAdmin.Models;
+using Bware.Data;
+using BwareAdmin.Areas.Admin.Adapter.Interface;
+
+namespace BwareAdmin.Areas.Admin.Adapter.Data
+{
+    public class UserAdapter : IUserAdapter
+    {
+        public IEnumerable<UserViewModel> getAllUsers()
+        {
+            var db = new Bware.Data.Model.BwareContext();
+
+            return db.AspNetUsers.Select(u => new UserViewModel { UserName = u.UserName }).ToList();
+        }
+    }
+}
---
+++ BwareAdmin/Areas/Admin/Adapter/Interface/IUserAdapter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BwareAdmin.Models;
+
+namespace BwareAdmin.Areas.Admin.Adapter.Interface
+{
+    interface IUserAdapter
+    {
+        IEnumerable<UserViewModel> getAllUsers();
+    }
+}
